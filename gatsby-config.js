ee0f66c6493dd7799df6bfbdb85dc534f4126f7b
qhtjd2131 <qhtjd2131@gmail.com>
2022-01-03 00:30:27 +0900

    custom : font change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Bosung's Blog`,
+    title: `Bosung's Blog`,
     author: {
       name: `Choi Bosung`,
       summary: `who lives Korea.  `,
