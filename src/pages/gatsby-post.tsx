3d1047fe7d3730b54ec2db32eb29e110f9d57ad7
qhtjd2131 <qhtjd2131@gmail.com>
2022-02-08 18:21:16 +0900

    branch test

--- src/pages/gatsby-post.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import styled from "styled-components";
-
-// const GatsbyPost = () => {
-
-//     return <div>gatsby post tag page</div>
-// }
-
-// export default GatsbyPost;
